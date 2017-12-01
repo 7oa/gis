1dfcd50c88ecee8d60819b768177eda87ecaa50c
Ann Semenova <semenova.7oa@yandex.ru>
2017-12-01 18:29:45 +0300

    new

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -3,5 +3,17 @@ $(document).ready(function() {
     //TODO: переделать ховеры объектов на главной
 	$('.obj').hover(function () {
 		$(this).addClass('active').siblings().removeClass("active");
-    })
+    });
+
+    $(".js-burger").click(function(){
+        $(".js-menu").addClass("open");
+    });
+    $(".js-close").click(function(){
+        $(".js-menu").removeClass("open");
+    });
+    $(window).on('load resize',function(){
+        if ($(window).width() >= '1280'){
+            $(".js-menu").removeClass("open");
+        }
+    });
 });
