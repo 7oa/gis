5e0a10f0def79d25566014eccffbdc6ce0044f0d
Ann Semenova <semenova.7oa@yandex.ru>
2017-12-08 13:45:20 +0300

    new

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -8,9 +8,11 @@ $(document).ready(function() {
     $(".js-burger").click(function(){
         $(".js-menu").addClass("open");
     });
+
     $(".js-close").click(function(){
         $(".js-menu").removeClass("open");
     });
+
     $(window).on('load resize',function(){
         if ($(window).width() >= '1280'){
             $(".js-menu").removeClass("open");
@@ -20,9 +22,18 @@ $(document).ready(function() {
     var swiper = new Swiper('.paretners-slider', {
         navigation: {
             nextEl: '.paretners-slider-next',
-            prevEl: '.paretners-slider-prev',
-        },
+            prevEl: '.paretners-slider-prev'
+        }
     });
+
+    var swiper = new Swiper('.diplom-slider', {
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.diplom-slider-next',
+            prevEl: '.diplom-slider-prev'
+        }
+    });
+
     var swiper = new Swiper('.sertificates-slider', {
         slidesPerView: 5,
         spaceBetween: 0,
@@ -45,16 +56,30 @@ $(document).ready(function() {
             prevEl: '.sertificates-slider-prev'
         }
     });
+
     $('.gis-list').masonry({
         itemSelector: '.gis-item',
         fitWidth: true,
         gutter: 20
     });
+
     $('.js-select').click(function(){
         $(this).next().slideToggle();
     });
+
     $('.js-select-list a').click(function(){
         var val = $(this).text();
         $('.js-select').text(val).next().slideUp();
+    });
+
+    $(".js-more").click(function () {
+        var txt = $(this).data('val');
+        if(!$(this).parent().hasClass("hide")){
+            txt = "Свернуть"
+        }
+
+        $(this).text(txt)
+            .parent().toggleClass("hide")
+            .prev().toggleClass("open");
     });
 });
